48c438a5ff2daac08a5781e78dd0fca11855ec9b
Abdulmumin Yaqeen <avdorr12345@gmail.com>
2023-09-01 16:29:27 +0100

    accordion

--- src/components/innerNav.svelte
+++ src/components/innerNav.svelte
@@ -1,16 +1,22 @@
 <script>
 	import Fa from 'svelte-fa';
 	import { faGithub } from '@fortawesome/free-brands-svg-icons';
+	import logo from '$lib/logo.png';
 </script>
 
 <div class="h-[12vh] p-4 md:p-6 w-full bg-inherit">
 	<ul class="flex w-full justify-between items-center">
-		<li class="text-xl px-3 p-1 bg-sky-100 rounded-3xl">SnippetsLand</li>
+		<li
+			class="text-xl px-3 py-2 gap-3 text-light bg-secondary-dark flex items-center justify-center rounded-3xl"
+		>
+			<img src={logo} class="h-6 w-6" alt="snippetsLand" />
+			SnippetLand
+		</li>
 		<li>
 			<ul class="flex gap-2">
-				<li class="bg-sky-100 rounded-full p-3">
+				<li class="bg-secondary-dark text-light rounded-full p-3">
 					<a href="https://github.com/Abdulmumin1/snippets" target="_blank">
-						<Fa icon={faGithub} /></a
+						<Fa icon={faGithub} class="" /></a
 					>
 				</li>
 			</ul>
---
+++ src/components/landing/sectionImg.svelte
@@ -0,0 +1,3 @@
+<div class="h-full">
+	<slot />
+</div>
