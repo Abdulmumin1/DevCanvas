9ebf8ec862b7cdfc55bd802331b1f973ffc752f8
Abdulmumin Yaqeen <avdorr12345@gmail.com>
2023-12-21 20:24:03 +0100

    fix: fork/save

--- src/routes/db/fe/fork/+server.js
+++ src/routes/db/fe/fork/+server.js
@@ -13,13 +13,16 @@ export async function POST({ locals: { supabase, getSession }, request }) {
 	let html = body.html;
 	let css = body.css;
 	let js = body.js;
+	let plugins = [JSON.parse(body.plugins)];
+	let config = JSON.parse(body.config);
+
 	let description =
 		body.description == 'Untitled Canvas' ? body.description : `Fork: ${body.description}`;
 	let user_id = session.user.id;
 	// console.log(session.user);
 	const { data, error: err } = await supabase
 		.from('htmlPlayground')
-		.insert([{ html, css, js, project_key: key, user_id, description }]);
+		.insert([{ html, css, js, project_key: key, user_id, description, plugins, config }]);
 
 	if (err) {
 		console.log(data);
