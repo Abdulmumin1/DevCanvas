1508904d2d8b4cc601bcd05db30ec9b0a96aaf26
Abdulmumin Yaqeen <avdorr12345@gmail.com>
2023-12-28 17:26:26 +0100

    editor setting, dropdown => sidenav

--- src/routes/play/[slug]/preview/+page.svelte
+++ src/routes/play/[slug]/preview/+page.svelte
@@ -5,11 +5,106 @@
 <script>
 	import { onMount } from 'svelte';
 	import { compileSassString } from '$lib/utils.js';
-
+	import {
+		fontawesomeLINK,
+		materialiconsLINK,
+		bootstrapLINK,
+		setup_js_plugin
+	} from '$lib/plugins/store.js';
 	import { page } from '$app/stores';
 	export let data;
 
 	let iframe;
+	function injectCSSPlugins(iframeDoc, cssPluginsVar) {
+		const tailwindScriptHTML = iframeDoc.getElementById('tailwincssDSFE4o431!!');
+		const fontawesomeHTML = iframeDoc.getElementById('fontawesomeDSFE4o431!!');
+		const materialiconsHTML = iframeDoc.getElementById('materialiconsDSFE4o431!!');
+		const bootstrapHTML = iframeDoc.getElementById('bootstrapDSFE4o431!!');
+
+		if (cssPluginsVar.fontawesome) {
+			// Create a temporary HTML element
+			const tempElement = document.createElement('div');
+
+			// Set the HTML code as text
+			tempElement.innerHTML = fontawesomeLINK;
+			// Extract the first child element (the <link> element) from the temporary element
+			let fontawesome = tempElement.firstElementChild;
+
+			// check if it exists
+
+			if (!fontawesomeHTML) {
+				iframeDoc.head.appendChild(fontawesome);
+			}
+		} else {
+			if (fontawesomeHTML) {
+				fontawesomeHTML.remove();
+			}
+		}
+
+		if (cssPluginsVar.bootstrap) {
+			// Create a temporary HTML element
+			const tempElement = document.createElement('div');
+
+			// Set the HTML code as text
+			tempElement.innerHTML = bootstrapLINK;
+			// Extract the first child element (the <link> element) from the temporary element
+			let bootstrap = tempElement.firstElementChild;
+
+			// check if it exists
+
+			if (!bootstrapHTML) {
+				iframeDoc.head.appendChild(bootstrap);
+			}
+		} else {
+			if (bootstrapHTML) {
+				bootstrapHTML.remove();
+			}
+		}
+
+		if (cssPluginsVar.materialicons) {
+			// Create a temporary HTML element
+			const tempElement = document.createElement('div');
+
+			// Set the HTML code as text
+			tempElement.innerHTML = materialiconsLINK;
+			// Extract the first child element (the <link> element) from the temporary element
+			let materialicons = tempElement.firstElementChild;
+			materialicons.id = 'materialiconsDSFE4o431!!';
+
+			// check if it exists
+			if (!materialiconsHTML) {
+				iframeDoc.head.appendChild(materialicons);
+			}
+		} else {
+			if (materialiconsHTML) {
+				materialiconsHTML.remove();
+			}
+		}
+
+		if (cssPluginsVar.tailwind) {
+			const tailwindScript = iframeDoc.createElement('script');
+			tailwindScript.src = 'https://cdn.tailwindcss.com';
+			tailwindScript.id = 'tailwincssDSFE4o431!!';
+
+			if (!tailwindScriptHTML) {
+				iframeDoc.body.appendChild(tailwindScript);
+			}
+		} else {
+			if (tailwindScriptHTML) {
+				tailwindScriptHTML.remove();
+			}
+		}
+
+		console.log('inject output');
+	}
+	function injectJSPlugins(iframeDoc, jsPluginsVar) {
+		let array = Object.keys(jsPluginsVar);
+		console.log(array);
+		// console.log(array);
+		for (let index = 0; index < array.length; index++) {
+			setup_js_plugin(array[index], jsPluginsVar, iframeDoc);
+		}
+	}
 
 	let zoomOut = $page.url.searchParams.get('preview');
 
@@ -55,6 +150,9 @@
 
 			if (!zoomOut) {
 				// Step 3: Create and append JavaScript code
+				let js = data.details.js;
+				injectJSPlugins(iframeDoc, data.details.plugins[0].js);
+				injectCSSPlugins(iframeDoc, data.details.plugins[0].css);
 				const scriptElement = iframeDoc.createElement('script');
 				scriptElement.textContent = `${js}`;
 				iframeDoc.body.appendChild(scriptElement);
