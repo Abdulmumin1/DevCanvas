2f76c841bb8018cbdfc6a9f2e4b8652e65a00fa3
Abdulmumin Yaqeen <avdorr12345@gmail.com>
2024-04-20 06:47:26 +0100

    enhacement: moved blog to custom cms

--- src/routes/blog/[slug]/+page.js
+++ src/routes/blog/[slug]/+page.js
@@ -12,34 +12,34 @@ export async function load({ params }) {
 	try {
 		//const post = await import(`../posts/${params.slug}.md`);
 		//console.log(import.meta.env.VITE_PUBLIC_DEVS_BLOG)
-		let test = 'http://localhost:5174';
-		let prod = 'https://devcanvas-blog-posts.pages.dev';
-		let test_key = 'demo_key';
-		let prod_key = import.meta.env.VITE_PUBLIC_DEVS_BLOG;
+		let test = 'http://localhost:5174'
+	let prod = 'https://devcanvas-blog-posts.pages.dev'
+	let test_key = 'demo_key'
+	let prod_key = import.meta.env.VITE_PUBLIC_DEVS_BLOG
 		const response = await fetch(`${prod}/api/posts/${params.slug}`, {
-			method: 'GET',
-			headers: {
-				'x-api-key': prod_key,
-				Accept: 'application/json',
-				'Content-Type': 'application/json'
-			}
-		});
-		if (response.ok) {
-			const post = await response.json();
-			//console.log(post)
-			return {
-				content: post.content.html,
-				meta: post.meta
-			};
+		method: 'GET',
+		headers: {
+			'x-api-key': prod_key,
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
 		}
-
+	});
+	    if(response.ok){
+	    const post = await response.json();
+	    //console.log(post)
+	    return {
+			content: post.content.html,
+			meta: post.meta
+		};
+	    }
+		
 		// console.log(params);
 		// return {
 		// 	content: params.content,
 		// 	meta: params.meta
 		// };
 	} catch (e) {
-		console.log(e);
+	    console.log(e)
 		throw error('404', `could not find the page you're lokking for!`);
 	}
 }
