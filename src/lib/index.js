80b1bb196855dd90ebe8d27e0d0d2af0a17bfbcd
Abdulmumin Yaqeen <avdorr12345@gmail.com>
2024-05-07 14:35:33 +0100

    fix: og

--- src/lib/index.js
+++ src/lib/index.js
@@ -18,8 +18,7 @@ export const SnippetsDescription = readable({
 	name: 'DevCanvas - Super Fast Online Editor',
 	des: 'an online editor with super powers, fast preview, plugins and super cool library of cool Frontend creations for your inspiration.',
 	url: 'https://devcanvas.art',
-	imageUrl:
-		'https://raw.githubusercontent.com/Abdulmumin1/snippetLand/6643c0d26744f6231aa168bf352c27b49664ba66/static/devCanvas.png'
+	imageUrl: '/devcanvas.png'
 });
 
 export const pageCountPl = writable(12);
