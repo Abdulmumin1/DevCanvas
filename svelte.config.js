84d10f323cfd1647886bba8ee90d16af23659a04
Abdulmumin Yaqeen <abdulmuminyqn@gmail.com>
2025-10-14 22:23:21 +0100

    fix: cloudflare config

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 // import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
